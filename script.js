f7a98827514c8fb07d8c52e48ab8ce92a47dd56b
priya-TECH74 <priyamythili74@gmail.com>
2026-01-07 11:29:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-
 let likes = [0, 0];
 
-function likePost(index) {
-    likes[index]++;
-    document.getElementById("like" + index).innerText =
-        likes[index] + " Likes";
+function login() {
+    let user = document.getElementById("username").value;
+    if (user === "") {
+        alert("Enter username");
+    } else {
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("feed").classList.remove("hidden");
+    }
 }
+
+function likePost(i) {
+    likes[i]++;
+    document.getElementById("like" + i).innerText = likes[i];
+}
+
+function commentPost() {
+    let comment = prompt("Enter your comment");
+    if (comment) {
+        alert("Comment posted: " + comment);
+    }
+}
+
+function sharePost() {
+    alert("Post shared successfully!");
+}
+
+
